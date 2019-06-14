3211e40cc39f9c932c1f958221b8baa84d300753
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-06-13 23:25:06 -0300

    Inserting Todo reducer.

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { Provider } from 'react-redux';
 import store from './store';
 
+// Components
+import TodoList from './TodoList';
+
 const App = () => (
   <Provider store={store}>
-    <h1>Hello App</h1>
+    <TodoList />
   </Provider>
 );
 
