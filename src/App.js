d9b8c8b32af38f7d0b13f7448b7ff680faa6d31d
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-06-14 00:27:10 -0300

    Setting Reactotron

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './config/ReactotronConfig';
 import { Provider } from 'react-redux';
 import store from './store';
 
 // Components
 import TodoList from './TodoList';
 
+console.tron.log('testando');
 const App = () => (
   <Provider store={store}>
     <TodoList />
