c8cc1ee905c976f2720728500af846ce4a342fd7
Lucas Ramos Romagnoli <lucasr.romagnoli@gmail.com>
2019-06-17 23:16:43 -0300

    Configuring react-saga

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,18 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
+import sagas from './sagas';
+
+const sagaMiddlewares = createSagaMiddleware();
+const middlewares = [sagaMiddlewares];
 
 const composer = process.env.NODE_ENV === 'development'
   ? compose(
-    applyMiddleware(...[]),
+    applyMiddleware(...middlewares),
     console.tron.createEnhancer(),
   )
   : applyMiddleware(...[]);
 
 const store = createStore(reducers, composer);
-
+sagaMiddlewares.run(sagas);
 export default store;
